a61db97a28fb8ba70b0160ee0653261204080167
Ira Tsypliaeva <irapodobina90@gmail.com>
2017-04-10 22:55:27 +0300

    Task completed

--- js/pictures.js
+++ js/pictures.js
@@ -50,7 +50,7 @@ function fillUploadOverlay(pic) {
   uploadOverlay.className += ' invisible';
 
   var galleryOverlay = document.querySelector('.gallery-overlay');
-  galleryOverlay.classList.remove('invisible');
+  // galleryOverlay.classList.remove('invisible');
 
   var image = galleryOverlay.querySelector('.gallery-overlay-image');
   image.setAttribute('src', pic.url);
@@ -63,3 +63,159 @@ function fillUploadOverlay(pic) {
 }
 
 fillUploadOverlay(pics[0]);
+
+/*
+*/
+var picture = document.querySelector('.picture');
+var galleryOverlay = document.querySelector('.gallery-overlay');
+var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
+
+/*
+Когда галерея открыта, то клавиша ESC должна закрывать галерею
+*/
+var onEscPress = function (evt) {
+  if (evt.keyCode === 27) {
+    closePopup();
+  }
+};
+
+var closePopup = function () {
+  galleryOverlay.classList.add('invisible');
+  document.removeEventListener('keydown', onEscPress);
+};
+
+/*
+При нажатии на любой из элементов .picture должен показываться элемент .gallery-overlay с подробным описанием картинки
+*/
+picture.addEventListener('click', function () {
+  openPopup();
+});
+
+var openPopup = function () {
+  galleryOverlay.classList.remove('invisible');
+  document.addEventListener('keydown', onEscPress);
+};
+
+/*
+При нажатии на элемент .gallery-overlay-close элемент .gallery-overlay должен скрываться
+*/
+galleryOverlayClose.addEventListener('click', function (evt) {
+  closePopup();
+  // evt.stopPropagation();
+});
+
+/*
+Если галерея открыта и фокус находится на крестике, то нажатие клавиши ENTER приводит к закрытию галереи
+*/
+var onEnterPressed = function (evt) {
+  if (evt.keyCode === 13) {
+    closePopup();
+  }
+};
+
+galleryOverlayClose.addEventListener('focus', function (evt) {
+  document.addEventListener('keydown', onEnterPressed);
+});
+galleryOverlayClose.addEventListener('blur', function (evt) {
+  document.removeEventListener('keydown', onEnterPressed);
+});
+
+/*
+Когда миниатюра в фокусе .picture, то галерея с увеличенным изображением должна показываться по нажатию кнопки ENTER
+Необходимо не забыть добавить tabindex='0' для картинки, чтобы элемент фокусировался
+*/
+var ENTER_KEY_CODE = 13;
+
+var isActivationEvent = function (evt) {
+  return evt.keyCode === ENTER_KEY_CODE;
+};
+galleryOverlay.addEventListener('keydown', function (evt) {
+  if (isActivationEvent(evt)) {
+    openPopup();
+  }
+});
+galleryOverlayClose.addEventListener('keydown', function (evt) {
+  if (isActivationEvent(evt)) {
+    closePopup();
+  }
+});
+
+/*
+При изменении значения поля загрузки фотографии #upload-file в форме #upload-select-image, показывается форма кадрирования изображения, а форма загрузки скрывается
+*/
+var uploadSelectImage = document.querySelector('#upload-select-image');
+var uploadFile = uploadSelectImage.querySelector('#upload-file');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var commentsFocused = false;
+
+
+var openCropForm = function () {
+  uploadSelectImage.classList.add('invisible');
+  uploadOverlay.classList.remove('invisible');
+  document.addEventListener('keydown', onCropFormEsc);
+};
+uploadFile.addEventListener('change', function (evt) {
+  openCropForm();
+});
+
+/*
+При нажатии на кнопку .upload-form-cancel форма кадрирования закрывается и показывается форма загрузки изображения
+*/
+var uploadFormCancel = uploadOverlay.querySelector('.upload-form-cancel');
+
+var closeCropForm = function () {
+  uploadSelectImage.classList.remove('invisible');
+  uploadOverlay.classList.add('invisible');
+  document.removeEventListener('keydown', onCropFormEsc);
+};
+
+uploadFormCancel.addEventListener('click', function () {
+  closeCropForm();
+});
+
+/*
+Когда форма кадрирования открыта, то клавиша ESC должна скрывать форму
+*/
+var onCropFormEsc = function (evt) {
+  // Если фокус находится на форме ввода комментария, то форма закрываться не должна
+  if (commentsFocused) {
+    return;
+  }
+
+  if (evt.keyCode === 27) {
+    closeCropForm();
+  }
+};
+
+/*
+Если фокус находится на форме ввода комментария, то форма закрываться не должна
+*/
+var uploadFormDescription = document.querySelector('.upload-form-description');
+uploadFormDescription.addEventListener('focus', function () {
+  commentsFocused = true;
+});
+uploadFormDescription.addEventListener('blur', function () {
+  commentsFocused = false;
+});
+
+/*
+Если форма открыта и фокус находится на крестике, то нажатие клавиши ENTER приводит к закрытию формы
+Если форма открыта и фокус находится на кнопке «Отправить», нажатие на ENTER приводит к закрытию формы
+
+*/
+var onFormEnter = function (evt) {
+  if (evt.keyCode === 13) {
+    closeCropForm();
+  }
+};
+
+uploadFormCancel.addEventListener('focus', function () {
+  document.addEventListener('keydown', onFormEnter);
+});
+uploadFormCancel.addEventListener('blur', function () {
+  document.removeEventListener('keydown', onFormEnter);
+});
+
+/*
+*/
+openPopup();
